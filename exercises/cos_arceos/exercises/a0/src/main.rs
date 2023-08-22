abddb4f2782f90cf0cce2552fddbed963c46d5d5
root <root@den>
2023-08-22 13:40:54 +0800

    a0

--- exercises/cos_arceos/exercises/a0/src/main.rs
+++ exercises/cos_arceos/exercises/a0/src/main.rs
@@ -8,7 +8,13 @@ use axstd::println;
 
 // TODO: Implement macro println_prefix.
 #[cfg(feature = "axstd")]
-use axstd::println_prefix;
+#[macro_export]
+macro_rules! println_prefix {
+    ($prefix:expr, $($args:tt)*) => {
+        axstd::print!("{}{}", $prefix, format_args!($($args)*));
+        axstd::println!();
+    };
+}
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
@@ -19,3 +25,17 @@ fn main() {
 
     println!("\n[ArceOS Tutorial]: A0 okay!");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
